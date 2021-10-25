6fa424478b96ebf835e5d055750c58bb449706a6
Starovoytov Alexandr <saniy200311@gmail.com>
2021-10-25 21:14:55 +0300

    change long to long long

--- polynom.c
+++ polynom.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long gorner(int n, long* a, long x) {
-    long ans = 0;
+long long gorner(int n, long long* a, long long x) {
+    long long ans = 0;
     for (int i = 0; i <= n; ++i) {
         ans = ans * x + a[i];
     }
     return ans;
 }
 
-long gorner_from_derivative(int n, long* a, long x) {
-    long ans = 0;
+long long gorner_from_derivative(int n, long long* a, long long x) {
+    long long ans = 0;
     for (int i = 0; i < n; ++i) {
         ans = ans * x + a[i] * (n - i);
     }
@@ -20,15 +20,15 @@ long gorner_from_derivative(int n, long* a, long x) {
 
 int main() {
     int n;
-    long x;
-    scanf("%d%ld", &n, &x);
-    long* a = malloc((n + 1) * (sizeof(long)));
+    long long x;
+    scanf("%d%Ld", &n, &x);
+    long long* a = malloc((n + 1) * (sizeof(long long)));
     for (int i = 0; i <= n; ++i) {
-        scanf("%ld", &a[i]);
+        scanf("%Ld", &a[i]);
     }
-    long ans = gorner(n, a, x);
-    long ans_derived = gorner_from_derivative(n, a, x);
-    printf("%ld\n%ld\n", ans, ans_derived);
+    long long ans = gorner(n, a, x);
+    long long ans_derived = gorner_from_derivative(n, a, x);
+    printf("%Ld\n%Ld\n", ans, ans_derived);
     free(a);
     return 0;
 }
