9a07ef4018b9fd971661f5a32bc6e98a7cf97335
Starovoytov Alexandr <saniy200311@gmail.com>
2021-10-25 21:16:39 +0300

    fix format flags

--- polynom.c
+++ polynom.c
@@ -21,14 +21,14 @@ long long gorner_from_derivative(int n, long long* a, long long x) {
 int main() {
     int n;
     long long x;
-    scanf("%d%Ld", &n, &x);
+    scanf("%d%lld", &n, &x);
     long long* a = malloc((n + 1) * (sizeof(long long)));
     for (int i = 0; i <= n; ++i) {
-        scanf("%Ld", &a[i]);
+        scanf("%lld", &a[i]);
     }
     long long ans = gorner(n, a, x);
     long long ans_derived = gorner_from_derivative(n, a, x);
-    printf("%Ld\n%Ld\n", ans, ans_derived);
+    printf("%lld\n%lld\n", ans, ans_derived);
     free(a);
     return 0;
 }
