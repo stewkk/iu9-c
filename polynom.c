3b2875198c6b174719553c84104a020babad7214
Starovoytov Alexandr <saniy200311@gmail.com>
2021-10-27 13:23:19 +0300

    fix

--- polynom.c
+++ polynom.c
@@ -21,14 +21,13 @@ long long gorner_from_derivative(unsigned int n, long long* a, long long x) {
 int main() {
     unsigned int n;
     long long x;
-    scanf("%ud%lld", &n, &x);
-    long long* a = malloc((n + 1) * (sizeof(long long)));
+    scanf("%u%lld", &n, &x);
+    long long a[n + 1];
     for (unsigned int i = 0; i <= n; ++i) {
         scanf("%lld", &a[i]);
     }
     long long ans = gorner(n, a, x);
     long long ans_derived = gorner_from_derivative(n, a, x);
     printf("%lld\n%lld\n", ans, ans_derived);
-    free(a);
     return 0;
 }
