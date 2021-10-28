c76eb3287aede7c4d850e1cce59eebe8aa2c01ef
Starovoytov Alexandr <saniy200311@gmail.com>
2021-10-28 22:20:29 +0300

    add wcount (task 1.14)

---
+++ wcount.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+int wcount(char* s) {
+    if (s[0] == '\0') {
+        return 0;
+    }
+    int ans = 0;
+    int index = 1;
+    while (s[index] != '\0') {
+        if (isspace(s[index]) && !isspace(s[index - 1])) {
+            ans++;
+        }
+        index++;
+    }
+    if (!isspace(s[index - 1])) {
+        ans++;
+    }
+    return ans;
+}
+
+int main() {
+    char str[100];
+    fgets(str, 100, stdin);
+    int ans = wcount(str);
+    printf("%d\n", ans);
+    return 0;
+}
