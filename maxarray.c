4c78168f80a183d7580b7e2c480341975476057b
Starovoytov Alexandr <saniy200311@gmail.com>
2021-10-28 18:50:05 +0300

    add maxarray (task 1.10)

---
+++ maxarray.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <inttypes.h>
+
+size_t maxarray(void* base, size_t nel, size_t width,
+             int (*compare)(void* a, void* b)) {
+    size_t ans = 0;
+    void* max = base;
+    uint8_t* cur = base;
+    for (size_t i = 1; i < nel; ++i) {
+        cur += width;
+        if ((*compare)(cur, max) > 0) {
+            ans = i;
+            max = cur;
+        }
+    }
+    return ans;
+}
+
+int cmp(void* a, void* b) {
+    int a_int = *(int*)a;
+    int b_int = *(int*)b;
+    if (a_int == b_int) {
+        return 0;
+    } else if (a_int > b_int) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 2, 1, 4, 2, 3}; // 5
+    size_t ans = maxarray(arr, 8, sizeof(int), cmp);
+    printf("%zu", ans);
+    return 0;
+}
