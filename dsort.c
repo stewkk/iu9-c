b66e30c5d6c5c39bdd22b571b006daa54e73254d
Starovoytov Alexandr <saniy200311@gmail.com>
2021-11-02 23:02:56 +0300

    add dsort (task 2.9)

---
+++ dsort.c
@@ -0,0 +1,35 @@
+
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+void dsort(char* str) {
+    unsigned int count[26] = {0};
+    char* p = str;
+    while (*p != '\0') {
+        count[*p - 'a']++;
+        p++;
+    }
+    for (int8_t i = 0; i < 26; ++i) {
+        while (count[i]) {
+            *str = (char)i + 'a';
+            str++;
+            count[i]--;
+        }
+    }
+}
+
+int main() {
+    char* str;
+    scanf("%ms", &str);
+    dsort(str);
+    printf("%s\n", str);
+    free(str);
+    return 0;
+}
