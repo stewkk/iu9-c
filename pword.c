80d8ff3c8a386e5c912a938ccd03985f810e8c46
Starovoytov Alexandr <saniy200311@gmail.com>
2021-11-05 00:58:42 +0300

    add pword (task 2.14)

---
+++ pword.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void preffunc(char* str, size_t* pref, size_t sz) {
+    pref[0] = 0;
+    for (size_t i = 1; i < sz; ++i) {
+        size_t t = pref[i - 1];
+        while (str[t] != str[i] && t > 0) {
+            t = pref[t - 1];
+        }
+        if (str[t] == str[i]) {
+            t++;
+        }
+        pref[i] = t;
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        return 0;
+    }
+    char* s = argv[1];
+    char* t = argv[2];
+    size_t s_sz = strlen(s);
+    size_t t_sz = strlen(t);
+    size_t* pref = calloc(s_sz, sizeof(size_t));
+    preffunc(s, pref, s_sz);
+    size_t p = 0;
+    for (size_t i = 0; i < t_sz; ++i) {
+        while (s[p] != t[i] && p > 0) {
+            p = pref[p - 1];
+        }
+        if (s[p] == t[i]) {
+            p++;
+        }
+        if (p == 0) {
+            printf("no\n");
+            free(pref);
+            return 0;
+        }
+    }
+    printf("yes\n");
+    free(pref);
+    return 0;
+}
