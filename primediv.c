1146b6ecf5c00ec9bf9e15bc21bd94e5b4498fa9
Starovoytov Alexandr <saniy200311@gmail.com>
2021-12-30 14:16:07 +0300

    fix primediv

--- primediv.c
+++ primediv.c
@@ -5,16 +5,21 @@
 int main() {
     long x;
     scanf("%ld", &x);
-    int sieve_size = (int)(labs(x) / 2) + 1;
-    char* sieve = calloc((unsigned long)sieve_size, sizeof(char));
-    memset(sieve, 1, (unsigned long)sieve_size);
-    int ans = 1;
     char is_x_prime = 1;
-    /* sieve[0] = sieve[1] = 0; */
-    for (int i = 2; i < sieve_size; ++i) {
+    for (int i = 2; i < 47000; ++i) {
         if (x % i == 0) {
             is_x_prime = 0;
         }
+    }
+    if (is_x_prime) {
+        printf("%ld\n", labs(x));
+        return 0;
+    }
+    int sieve_size = (int)(labs(x) / 2) + 1;
+    char* sieve = malloc((unsigned long)sieve_size);
+    memset(sieve, 1, (unsigned long)sieve_size);
+    int ans = 1;
+    for (int i = 2; i < sieve_size; ++i) {
         if (sieve[i]) {
             if (x % i == 0) {
                 ans = i;
@@ -27,11 +32,7 @@ int main() {
             }
         }
     }
-    if (is_x_prime) {
-        printf("%ld\n", labs(x));
-    } else {
-        printf("%d\n", ans);
-    }
+    printf("%d\n", ans);
     free(sieve);
     return 0;
 }
