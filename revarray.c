89197410a8d7f723693295424ab26699b137187c
Starovoytov Alexandr <saniy200311@gmail.com>
2021-12-30 14:43:44 +0300

    fix revarray

--- revarray.c
+++ revarray.c
@@ -2,19 +2,23 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void swap(uint8_t* a, uint8_t* b) {
-    uint8_t temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(uint8_t* a, uint8_t* b, size_t width) {
+    for (size_t i = 0; i < width; ++i) {
+      uint8_t temp = *a;
+      *a = *b;
+      *b = temp;
+      a++;
+      b++;
+    }
 }
 
 void revarray(void* base, size_t nel, size_t width) {
     uint8_t* left = base;
     uint8_t* right = (uint8_t*)base + (nel - 1) * width;
     while (left < right) {
-        swap(left, right);
-        left++;
-        right--;
+        swap(left, right, width);
+        left += width;
+        right -= width;
     }
 }
 
