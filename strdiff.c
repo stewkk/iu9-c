d64a81150041ee40f0749727b840ad51f6ab1095
Starovoytov Alexandr <saniy200311@gmail.com>
2021-12-30 14:43:51 +0300

    fix strdiff

--- strdiff.c
+++ strdiff.c
@@ -3,24 +3,33 @@
 #include <inttypes.h>
 
 int strdiff(char* a, char* b) {
-    size_t index = 0;
-    while (a[index] != '\0' && b[index] != '\0') {
-        uint8_t mask = 1;
-        for (size_t i = 0; i < 8 * sizeof(char); ++i) {
-            if ((a[index] & mask) != (b[index] & mask)) {
-                return index * 8 * sizeof(char) + i;
-            }
-            mask <<= 1;
+    int32_t index = -1;
+    do {
+      index++;
+      uint8_t mask = 1;
+      for (size_t i = 0; i < 8 * sizeof(char); ++i) {
+        if ((a[index] & mask) != (b[index] & mask)) {
+          return index * 8 * sizeof(char) + i;
         }
-        index++;
-    }
+        mask <<= 1;
+      }
+    } while (a[index] != '\0' && b[index] != '\0');
     return -1;
 }
 
 int main() {
-    printf("%d\n%d\n%d\n",
-           strdiff("aa", "ai"),
-           strdiff("abcd", "abcd"),
-           strdiff("ba", "aa"));
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",
+           strdiff("", "The quick brown fox jumps over the lazy dog"),
+           strdiff("T", "The quick brown fox jumps over the lazy dog"),
+           strdiff("The quick ", "The quick brown fox jumps over the lazy dog"),
+           strdiff("The quick brown fox ", "The quick brown fox jumps over the lazy dog"),
+           strdiff("The quick brown fox jumps over the lazy do", "The quick brown fox jumps over the lazy dog"),
+           strdiff("The quick brown fox jumps over the lazy dog", ""),
+           strdiff("The quick brown fox jumps over the lazy dog", "T"),
+           strdiff("The quick brown fox jumps over the lazy dog", "The quick "),
+           strdiff("The quick brown fox jumps over the lazy dog", "The quick brown fox "),
+           strdiff("The quick brown fox jumps over the lazy dog", "The quick brown fox jumps over the lazy dog"),
+           strdiff("", ""),
+           strdiff("The quick brown fox jumps over the lazy dog", "The quick brown fox jumps over the lazy do"));
     return 0;
 }
